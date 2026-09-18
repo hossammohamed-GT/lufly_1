f1c7f8be4e7d412b866c27e8c1abe90352f08785
hossammohamed-GT <328752924+hossammohamed-GT@users.noreply.github.com>
2026-09-18 11:31:50 +0000

    Clean preview files and stabilize preloader motion with zero vertical jitter
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- resources/views/components/loader.php
+++ resources/views/components/loader.php
@@ -44,38 +44,40 @@ $foot = $foots[$locale] ?? $foots['en'];
     <div class="ld-blob b"></div>
     <div class="ld-vignette"></div>
 
-    <div class="ld-kicker"><?= e($kicker) ?></div>
+    <div class="ld-center">
+        <div class="ld-kicker"><?= e($kicker) ?></div>
 
-    <div class="ld-logoBox" id="ldLogoBox">
-        <!-- stroke-draw sketch (hand-tuned to the brand mark) -->
-        <svg class="ld-draw" viewBox="0 0 510 325" aria-hidden="true">
-            <path pathLength="1" d="M 30 16 L 30 298 Q 30 306 38 306 L 116 306"/>
-            <path pathLength="1" d="M 64 16 L 64 174"/>
-            <path pathLength="1" d="M 64 244 L 116 244"/>
-            <path pathLength="1" d="M 158 148 L 158 246 Q 158 292 198 292 Q 238 292 238 246 L 238 148"/>
-            <path pathLength="1" d="M 288 292 L 288 82 Q 288 42 330 42"/>
-            <path pathLength="1" d="M 256 148 L 320 148"/>
-            <path pathLength="1" d="M 378 16 L 378 292"/>
-            <path pathLength="1" d="M 416 148 L 456 238 L 496 148 L 424 306"/>
-        </svg>
+        <div class="ld-logoBox" id="ldLogoBox">
+            <!-- stroke-draw sketch (hand-tuned to the brand mark) -->
+            <svg class="ld-draw" viewBox="0 0 510 325" aria-hidden="true">
+                <path pathLength="1" d="M 30 16 L 30 298 Q 30 306 38 306 L 116 306"/>
+                <path pathLength="1" d="M 64 16 L 64 174"/>
+                <path pathLength="1" d="M 64 244 L 116 244"/>
+                <path pathLength="1" d="M 158 148 L 158 246 Q 158 292 198 292 Q 238 292 238 246 L 238 148"/>
+                <path pathLength="1" d="M 288 292 L 288 82 Q 288 42 330 42"/>
+                <path pathLength="1" d="M 256 148 L 320 148"/>
+                <path pathLength="1" d="M 378 16 L 378 292"/>
+                <path pathLength="1" d="M 416 148 L 456 238 L 496 148 L 424 306"/>
+            </svg>
 
-        <!-- water, masked to the real glyph shapes -->
-        <div class="ld-water" aria-hidden="true">
-            <div class="ld-waterBody" id="ldWaterBody">
-                <span class="wave"></span>
-                <span class="wave w2"></span>
+            <!-- water, masked to the real glyph shapes -->
+            <div class="ld-water" aria-hidden="true">
+                <div class="ld-waterBody" id="ldWaterBody">
+                    <span class="wave"></span>
+                    <span class="wave w2"></span>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="ld-tag" id="ldTag"><?= e($tag) ?></div>
+        <div class="ld-tag" id="ldTag"><?= e($tag) ?></div>
 
-    <div class="ld-ui" id="ldUi">
-        <div class="ld-row">
-            <div class="ld-track"><span class="ld-fill" id="ldFill"></span></div>
-            <span class="ld-pct" id="ldPct">0%</span>
+        <div class="ld-ui" id="ldUi">
+            <div class="ld-row">
+                <div class="ld-track"><span class="ld-fill" id="ldFill"></span></div>
+                <span class="ld-pct" id="ldPct">0%</span>
+            </div>
+            <div class="ld-msg" id="ldMsg"><?= e($msg) ?></div>
         </div>
-        <div class="ld-msg" id="ldMsg"><?= e($msg) ?></div>
     </div>
 
     <div class="ld-foot"><?= e($foot) ?></div>
