f699d874ae11a5f2c98cdf694f76d44df7c8c2ee
vdtafury <178310976+vdtafury@users.noreply.github.com>
2026-09-22 03:33:27 +0300

    fix(security): apply security headers middleware uniformly to api routes

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
 
     'middleware_groups' => [
         'web' => ['locale', 'csrf', 'security'],
-        'api' => ['locale', 'csrf', 'api.log'],
+        'api' => ['locale', 'security', 'csrf', 'api.log'],
     ],
 
     'middleware_aliases' => [
